92981748082a68e6973110f85eae906f37038aea
muhmmdfath <fatihadli709@gmail.com>
2025-01-29 14:57:53 +0700

    add policy create, update, and delete for admin user

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Policies\UserPolicy;
+use App\Models\User;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        // Contoh penggunaan Gate
+        Gate::define('view-dashboard', function ($user) {
+            return $user->role === 'admin';
+        });
+    }
+}
